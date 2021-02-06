360af00bffb4562c560e616af264939c3513c71c
Polina Ingeroinen <71010265+IngeroinenPolina@users.noreply.github.com>
2021-02-07 00:31:10 +0300

    Add files via upload

--- homework03/life_console.py
+++ homework03/life_console.py
@@ -1,18 +1,19 @@
 import curses
-import curses.textpad
-import sys
 from life import GameOfLife
 from ui import UI
+from time import sleep
+
 
 class Console(UI):
+
     def __init__(self, life: GameOfLife) -> None:
         super().__init__(life)
-        self.speed = speed
-    def draw_borders(self, screen) -> None:
-        """ Отобразить рамку. """
-        curses.textpad.rectangle(self.screen, 0, 0, self.life.rows + 1, self.life.cols + 1)
+        self.screen = curses.initscr()
 
-    def draw_grid(self, screen) -> None:
+    def draw_borders(self) -> None:
+        self.screen.border(0)
+
+    def draw_grid(self) -> None:
         """ Отобразить состояние клеток. """
         for i in range(self.life.rows):
             for j in range(self.life.cols):
@@ -22,24 +23,22 @@ class Console(UI):
                     self.screen.addch(i + 1, j + 1, ' ')
 
     def run(self) -> None:
-        self.screen = curses.initscr()
-        curses.noecho()
-        curses.cbreak()
-
         self.draw_borders()
 
         running = True
         while running:
-            self.screen.erase()
             self.draw_borders()
             self.draw_grid()
             self.screen.refresh()
-
-            sleep(1 / self.speed)
+            
+            sleep(0.5)
 
             self.life.step()
+
         curses.endwin()
+
+
 if __name__ == "__main__":
-    game = GameOfLife(size=(10,10))
+    game = GameOfLife(size=(10, 10))
     console = Console(game)
     console.run()
